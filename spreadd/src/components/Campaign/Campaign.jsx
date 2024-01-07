2a4ada3e73e6502756b319fca6d13575a292aeab
Bhautik <bhautik.kapadiya@adrixus.com>
2024-01-07 19:58:28 +0530

    Complete done of capaign

--- spreadd/src/components/Campaign/Campaign.jsx
+++ spreadd/src/components/Campaign/Campaign.jsx
@@ -11,7 +11,15 @@ import {
   StyledPopUpHeading,
   StyledImageWrapper,
   StyledImage,
-
+  StyledCardWrapper,
+  StyledCardIconWrapper,
+  StyledCardIcon,
+  StyledCardHeading,
+  StyledCardText,
+  StyledCampaignDetail,
+  StyledCampaign,
+  StyledCampaignHeading,
+  StyledCampaignText
 
 
 } from "./Campaign.styled";
@@ -26,18 +34,42 @@ export const Campaign = () => {
             <StyledLeftCampaign>
               <StyledPopUpBoxWrapper>
                 <StyledPopUpIconWrapper>
-                  <StyledPopUpIcon src="" alt="setting-icon"/>
+                  <StyledPopUpIcon src="https://spreadd.io/assets/home/setting.svg" alt="setting-icon"/>
                 </StyledPopUpIconWrapper>
                 <StyledPopUpHeading>
                   Available solutions
                 </StyledPopUpHeading>
               </StyledPopUpBoxWrapper>
+              
               <StyledImageWrapper>
                 <StyledImage src="https://spreadd.io/assets/home/marketplace.jpg" alt="marketplace-image"/>
               </StyledImageWrapper>
+
+              <StyledCardWrapper>
+                <StyledCardIconWrapper>
+                  <StyledCardIcon src="https://spreadd.io/assets/home/bulb.svg" alt="bulb-icon"/>
+                </StyledCardIconWrapper>
+                <StyledCardHeading>
+                  Easy Methods
+                </StyledCardHeading>
+                <StyledCardText>
+                  We Have the best ideas for your Brand
+                </StyledCardText>
+              </StyledCardWrapper>
             </StyledLeftCampaign>
 
             <StyledRightCampaign>
+              <StyledCampaignDetail>
+                <StyledCampaign>
+                  Campaign marketplace
+                </StyledCampaign>
+                <StyledCampaignHeading>
+                Participate in campaigns & build relationships with brands
+                </StyledCampaignHeading>
+                <StyledCampaignText>
+                Join our campaign marketplace to deliver content, build strong relationships with brands and receive agreement-based timely payouts.
+                </StyledCampaignText>
+              </StyledCampaignDetail>
             </StyledRightCampaign>
           </StyledInnerWrapper>
         </StyledWrapper>
--- spreadd/src/components/Campaign/Campaign.styled.js
+++ spreadd/src/components/Campaign/Campaign.styled.js
@@ -29,3 +29,157 @@ export const StyledInnerWrapper=styled.div`
     }
 `
 
+export const StyledLeftCampaign=styled.div`
+    position: relative;
+    max-width: 48%;
+    width: 100%;
+
+    @media screen and (max-width: 767px){
+    max-width: max-content;
+    margin-top: 30px;
+    }
+`
+
+export const StyledPopUpBoxWrapper=styled.div`
+    background: linear-gradient(90deg, rgba(255, 255, 255, 0.1) 0.46%, rgba(255, 255, 255, 0.1) 100%);
+    border: 1px solid rgba(255, 255, 255, 0.75);
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 3px 8px;
+    backdrop-filter: blur(12.2941px);
+    border-radius: 5px;
+    width: max-content;
+    padding: 15px 10px 10px;
+    position: absolute;
+    top: 10px;
+    bottom: unset;
+    left: 70%;
+
+    @media screen and (max-width: 991px) {
+    left: 60%;
+    }
+`
+
+export const StyledPopUpIconWrapper=styled.div`
+    position: absolute;
+    right: 30px;
+    top: -15px;
+`
+
+export const StyledPopUpIcon=styled.img`
+    vertical-align: middle;
+`
+
+export const StyledPopUpHeaeding=styled.h5`
+    font-weight: 600;
+    font-size: 1.3rem;
+    line-height: 1;
+    color: #ffffff;
+`
+
+export const StyledImageWrapper=styled.div`
+
+
+    @media screen and (max-width: 991px){
+    text-align: center;
+    }
+`
+
+export const StyledImage=styled.img`
+    max-width: 450px;
+    width: 100%;
+
+    @media screen and (max-width: 1280px){
+    max-width: 398px;
+    width: 90%;
+    }
+`
+
+export const StyledCardWrapper=styled.div`
+    background: linear-gradient(90deg, rgba(255, 255, 255, 0.1) 0.46%, rgba(255, 255, 255, 0.1) 100%);
+    border: 1px solid rgba(255, 255, 255, 0.75);
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 3px 8px;
+    backdrop-filter: blur(12.2941px);
+    border-radius: 5px;
+    width: max-content;
+    padding: 15px 10px 10px;
+    position: absolute;
+    bottom: -20px;
+    left: -20px;
+
+    @media screen and  (max-width: 991px){
+    left: 0;
+    }
+`
+
+export const StyledCardIconWrapper=styled.div`
+    position: absolute;
+    right: 30px;
+    top: -15px;
+`
+
+export const StyledCardIcon=styled.img`
+    vertical-align: middle;
+`
+
+export const StyledCardHeading=styled.h5`
+    font-weight: 600;
+    font-size: 1.3rem;
+    line-height: 1;
+    color: #ffffff;
+    margin-top: 0;
+    margin-bottom: 0.5em;
+`
+
+export const StyledCardText=styled.p`
+    margin: 0;
+    font-size: 1rem;
+    color: white;
+`
+
+
+export const StyledRightCampaign=styled.div`
+    max-width: 48%;
+    width: 100%;
+
+    @media screen and  (max-width: 767px){
+    max-width: 100%;  
+    }    
+`
+
+export const StyledCampaign=styled.div`
+    font-size: 1.5rem;
+    line-height: 1.5;
+    font-weight: 400;
+    background: linear-gradient(50deg, white, transparent);
+    padding: 8px 12px;
+    margin-bottom: 25px;
+    width: max-content;
+`
+
+export const StyledCampaignHeading=styled.h3`
+    font-size: 4.7rem;
+    font-weight: 400;
+    line-height: 1;
+    margin-bottom: 20px;
+    margin-top: 0;
+    color: #212b36;
+
+    @media screen and (max-width: 767px) {
+    font-size: 2.8rem;
+    line-height: 1;
+    }
+`
+
+export const StyledCampaignText=styled.p`
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 2;
+
+@media screen and (max-width: 767px){
+    font-size: 1rem;
+    line-height: 1.1;
+}
+@media screen and (min-width:768px) and (max-width: 991px){
+    font-size: 0.8rem;
+    line-height: 1.2;
+}
+`
